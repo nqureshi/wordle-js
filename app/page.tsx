83662879ca1faea3c520606a1d2edd85bd254a61
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2023-12-06 22:35:43 -0500

    disable input if win or loss

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,41 @@ import { NUM_OF_GUESSES_ALLOWED } from './utils/constants';
 import { range, sample } from './utils/utils';
 import { checkGuess } from './utils/game-helpers';
 import { WORDS } from './utils/data';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 let ANSWER = sample(WORDS);
-// console.log(ANSWER);
+console.log(ANSWER);
 
-function Form({ handleSubmit }) {
+function Form({ handleSubmit, guesses }: { handleSubmit: () => void; guesses: string[] }) {
 
   const [guess, setGuess] = useState('');
   const [error, setError] = useState('');
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const handleSubmitForm = (e: React.FormEvent) => {
     e.preventDefault(); // needed to stop the whole page from refreshing on Submit
-    if (guess.length === 5) {
-      handleSubmit(guess);
-      setGuess('')
-      setError('')
-    } else {
-      setError('Input must be 5 characters')
-    };
+
+    // length validation check
+    if (guess.length !== 5) {
+      setError('Input must be 5 characters');
+      return;
+    }
+
+    // check if Win, or Loss
+    if (guess.toUpperCase() === ANSWER) {
+      setIsDisabled(true); // Disable text input
+      console.log("You won!");
+      console.log(guesses);
+    } else if (guesses.length > NUM_OF_GUESSES_ALLOWED - 2) {
+      setIsDisabled(true);
+      console.log("You lost!");
+    }
+
+    // if guess is valid length + neither win nor loss...
+    handleSubmit(guess);
+    setGuess('');
+    setError('');
+
   };
 
   return (
@@ -35,6 +51,7 @@ function Form({ handleSubmit }) {
           value={guess}
           onChange={e => setGuess(e.target.value)}
           className="border-2 border-gray-300 rounded-md p-2 mb-2"
+          disabled={isDisabled}
         />
         {error && <p className="text-red-500">{error}</p>}
         <input type="submit" value="Submit" className="bg-blue-500 text-white rounded-md px-4 py-2 mt-4" />
@@ -81,7 +98,7 @@ function Game() {
     <>
       <div className="flex flex-col items-center justify-center bg-white p-24">
         <h1 className="text-4xl mb-4">WORDLE</h1>
-        <Form handleSubmit={handleSubmit} />
+        <Form handleSubmit={handleSubmit} guesses={guesses} />
         <div className="flex flex-col items-center justify-center bg-white p-4">
         </div>
         <h2>So far, you've guessed...</h2>
